121d115def2dcceab5e788fbf68d5b5aa2bd465e
emmanuel <oemmanuel.uwadiae@gmail.com>
2025-02-01 18:28:57 +0000

    adding ability to airdrop solana

--- airdrop/index.ts
+++ airdrop/index.ts
@@ -1,1 +1,24 @@
-export const airdrop = (address: string, amount: number) => {};
+import { PublicKey, Connection, LAMPORTS_PER_SOL } from "@solana/web3.js";
+
+// 10**9 = 1 sol
+
+export const airdrop = async (address: string, amount: number) => {
+  const publicKey = new PublicKey(address);
+
+  const conn = new Connection("http://localhost:8899", "confirmed");
+
+  const latestBlockHash = await conn.getLatestBlockhash();
+
+  const signature = await conn.requestAirdrop(
+    publicKey,
+    amount * LAMPORTS_PER_SOL
+  );
+  //wait for transaction of signature to be confirmed before exiting
+  await conn.confirmTransaction({
+    blockhash: latestBlockHash.blockhash,
+    lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+    signature,
+  });
+};
+
+airdrop("CV6JPu7JutiAqWnBASht3uzSqSpYLv3iHhWuHpLrMXE9", 6);
